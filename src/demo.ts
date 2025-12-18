c02f0494822f2603bf48de1b77f86ed9d8d2cc83
nadyavozna <Nadiia_Vozna@std.suitt.edu.ua>
2025-12-18 16:10:21 +0200

    feat: add basic types for utils (number/string)

--- src/demo.ts
+++ src/demo.ts
@@ -1,7 +1,4 @@
 import { add, capitalize } from './index';
 
-// ПОМИЛКА ЛІНТИНГУ навмисно: подвійні лапки
-
-console.log('sum(any):', add(2, 3));
-
-console.log('capitalize(any):', capitalize('hello'));
+console.log('sum(typed):', add(2, 3));
+console.log('capitalize(typed):', capitalize('hello'));
